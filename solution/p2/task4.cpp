dc0bf8322415b77b78ceb57e4812ff425be8a3f6
SHEN Jiamin <shen_jiamin@outlook.com>
2019-10-09 17:40:39 +0800

    change to cpp

--- solution/p2/task4.cpp
+++ solution/p2/task4.cpp
@@ -1,13 +1,29 @@
 #include "common.h"
+#include <iomanip>
+#include <sstream>
+
+using std::hex;
+using std::string;
+using std::stringstream;
+string to_hexstring(const string& str)
+{
+    stringstream ss;
+
+    for (uint8_t c : str) {
+        ss << hex << unsigned(c);
+    }
+
+    return ss.str();
+}
 
 int main(int argc, char const* argv[])
 {
     BN_CTX* ctx = BN_CTX_new();
     BIGNUM* n = load_hex("DCBFFE3E51F62E09CE7032E2677A78946A849DC4CDDE3A4D0CB81629242FB1A5");
     BIGNUM* e = load_hex("010001");
-    BIGNUM* M = load_hex("4120746f702073656372657421");
     BIGNUM* d = load_hex("74D806F9F3A62BAE331FFE3F0A68AFE35B3D2E4794148AACBC26AA381CD7D30D");
 
+    BIGNUM* M = load_hex(to_hexstring("A top secret!").c_str());
     BIGNUM* C = with_ctx3(BN_mod_exp, M, e, n, ctx);
     printBN("Ciphertext: ", C);
 
