3c4b5cf1254e565dcc586ebc743dba313c21cfa8
SHEN Jiamin <shen_jiamin@outlook.com>
2019-10-15 02:35:04 +0800

    p27

--- Part1/p2_7.c
+++ Part1/p2_7.c
@@ -17,8 +17,6 @@ int rsa_verify(BIGNUM* n, BIGNUM* e, BIGNUM* m, BIGNUM* s)
 
 void verify(BIGNUM* n, BIGNUM* e, const char* message, const char* signature)
 {
-    /* allocate and initialize a BN_CTX structure */
-    ctx = BN_CTX_new();
 
     /* load message from a ascii-encoded string */
     char message_hex[BUFFER_SIZE];
@@ -46,20 +44,29 @@ void verify(BIGNUM* n, BIGNUM* e, const char* message, const char* signature)
 
 int main(int argc, char const* argv[])
 {
-    BN_CTX* ctx = BN_CTX_new();
+    /* allocate and initialize a BN_CTX structure */
+    ctx = BN_CTX_new();
+
+    /* load rsa parameters form strings in hex format */
     BIGNUM* n = load_hex("AE1CD4DC432798D933779FBD46C6E1247F0CF1233595113AA51B450F18116115");
     BIGNUM* e = load_hex("010001");
 
+    /* verify the signature */
     const char* msg = "Launch a missile.";
     char sig[] = "643D6F34902D9C7EC90CB0B2BCA36C47FA37165C0005CAB026C0542CBDB6802F";
     verify(n, e, msg, sig);
 
     printf("\n");
 
+    /* modify the signature and verify again */
     sig[62] = '3';
-
     verify(n, e, msg, sig);
 
+    /* free the components of the BIGNUM and the structure itself */
+    BN_free(n);
+    BN_free(e);
+
+    /* free the components of the BN_CTX and the structure itself */
     BN_CTX_free(ctx);
     return 0;
 }
