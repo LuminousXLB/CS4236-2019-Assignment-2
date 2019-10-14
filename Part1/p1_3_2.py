03470a36828cf71462fcb5d42cf91327e1ca4e43
SHEN Jiamin <shen_jiamin@outlook.com>
2019-10-15 01:15:23 +0800

    p132

--- Part1/p1_3_2.py
+++ Part1/p1_3_2.py
@@ -7,9 +7,11 @@ def xor(bytes1: bytes, bytes2: bytes):
 
 
 if __name__ == "__main__":
+    # Known Information
     P1 = b'This is a known message!'
     C1 = unhexlify(b'a469b1c502c1cab966965e50425438e1bb1b5f9037a4c159')
     C2 = unhexlify(b'bf73bcd3509299d566c35b5d450337e1bb175f903fafc159')
 
-    P2 = xor(P1, xor(C1, C2))
+    # Calculate P2
+    P2 = xor(xor(P1, C1), C2)
     print(P2.decode('ascii'))
