effaaf657a3e35dcf4a28c8d16f5c28eb2efba0f
SHEN Jiamin <shen_jiamin@outlook.com>
2019-10-15 00:37:32 +0800

    add comments

--- Part1/p1_2.sh
+++ Part1/p1_2.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# Input file
+# Input File
 PICTURE=./picture/pic_original.bmp
 
-# Output file
+# Output File
 PIC_ECB=pic_ecb.bmp
 PIC_CBC=pic_cbc.bmp
 
@@ -11,8 +11,11 @@ PIC_CBC=pic_cbc.bmp
 echo "    (1) Encrypt the picture using AES-128-ECB"
 ################################################################################
 
+# Output the file header to the target file
 head -c 54 $PICTURE > $PIC_ECB
 
+# Encrypt the picture in AES-128-ECB mode
+# Output the encrypted data (except for the header part) to the target file
 openssl enc -aes-128-ecb \
     -K 00112233445566778889aabbccddeeff -e \
     -in $PICTURE | tail -c +55 >> $PIC_ECB
@@ -21,7 +24,11 @@ openssl enc -aes-128-ecb \
 echo "    (2) Encrypt the picture using AES-128-CBC"
 ################################################################################
 
+# Output the file header to the target file
 head -c 54 $PICTURE > $PIC_CBC
+
+# Encrypt the picture in AES-128-CBC mode
+# Output the encrypted data (except for the header part) to the target file
 openssl enc -aes-128-cbc \
     -K 00112233445566778889aabbccddeeff \
     -iv 01020304050607080102030405060708 -e \
